1cc897326d7ab87ab881398d45f29222c873d456
teodora-web <djordjeviccctedora2211@gmail.com>
2026-02-05 11:44:22 +0100

    Setup Laravel backend structure, models, migrations and routes

--- backend/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ backend/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-       Schema::create('roles', function (Blueprint $table) {
-    $table->id();
-    $table->string('name')->unique(); // student, admin
-    $table->timestamps();
-});
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
     }
 
     /**
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('password_reset_tokens');
     }
 };
